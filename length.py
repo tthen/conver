2e4ffe94240cdaa117d2e42a97ee7cec489d071c
tthen <85257109+tthen@users.noreply.github.com>
2026-01-02 18:14:36 -0500

    Create length.py

---
+++ length.py
@@ -0,0 +1,46 @@
+def feet_to_centimeters (feet):
+    return feet * 30.48
+
+def feet_to_inches (feet):
+    return feet * 12
+
+def feet_to_kilometers (feet):
+    return feet * 3.048E-04
+
+def feet_to_meters (feet):
+    return feet * 0.3048
+
+def feet_to_miles (feet):
+    return feet * 1.894E-04
+
+def inches_to_centimeters (inch):
+    return inch * 2.540
+
+def inches_to_feet (inch):
+    return inch * 0.08333
+
+def inches_to_kilometers (inch):
+    return inch * 2.54E-05
+
+def inches_to_meters (inch):
+    return inch * 0.0254
+
+def kilometers_to_feet (kilometers):
+    return kilometers * 3.2808E03
+
+def kilometers_to_meters (kilometers):
+    return kilometers * 1000
+
+def kilometers_to_miles (kilometers):
+    return kilometers * 0.62137
+
+def kilometers_to_yards (kilometers):
+    return kilometers * 1.0936E03
+
+def meters_to_feet (meters):
+    return meters * 3.2808
+
+
+
+
+
