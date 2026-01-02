96d532ec5c434c28c02fdef2924a20e57c689bbc
tthen <85257109+tthen@users.noreply.github.com>
2026-01-02 18:32:39 -0500

    Update length.py

--- length.py
+++ length.py
@@ -49,7 +49,68 @@ def micrometers_to_anstrong (micrometers):
 def micrometers_to_centimeters (micrometers):
     return micrometers * 1.0E-03
 
+def micrometers_to_feet (micrometers):
+    return micrometers * 3.2808E-06
 
+def micrometers_to_inches (micrometers):
+    return micrometers * 3.9370E-05
 
+def micrometers_to_meters (micrometers):
+    return micrometers * 1.0E-06
 
+def micrometers_to_millimeters (micrometers):
+    return micrometers * 0.001
 
+def micrometers_to_nanometers (micrometers):
+    return micrometers * 1000
+
+def miles_to_feet (miles):
+    return miles * 5280
+
+def miles_to_kilometers (miles):
+    return miles * 1.6093
+
+def miles_to_meters (miles):
+    return miles * 1.6093E03
+
+def miles_to_yards (miles):
+    return miles * 1760
+
+def millimeters_to_angstrom (millimeters):
+    return millimeters * 1.0E07
+
+def millimeters_to_centimeters (millimeters):
+    return millimeters * 0.1
+
+def millimeters_to_inches (millimeters):
+    return millimeters * 0.03937
+
+def millimeters_to_meters (millimeters):
+    return millimeters * 0.001
+
+def millimeters_to_micrometers (millimeters):
+    return millimeters * 1000
+
+def millimeters_to_mils (millimeters):
+    return millimeters * 39.37
+
+def nanometers_to_angstrom (nanometers):
+    return nanometers * 10
+
+def nanometers_to_centimeters (nanometers):
+    return nanometers * 1.0E-07
+
+def nanometers_to_inches (nanometers):
+    return nanometers * 3.937E-08
+
+def nanometers_to_micrometers (nanometers):
+    return nanometers * 0.001
+
+def nanometers_to_millimeters (nanometers):
+    return nanometers * 1.0E-06
+
+def yards_to_centimeters (yards):
+    return yards * 91.44
+
+def yards_to_meters (yards):
+    return yards * 0.9144
